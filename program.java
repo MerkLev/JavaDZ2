0f074ed07e49080230dddf068eafd6af35e8d46c
Lev <Dmslmrk@icloud.com>
2023-01-30 18:28:32 +0300

    done2

--- program.java
+++ program.java
@@ -7,26 +7,24 @@ import java.io.PrintWriter;
 public class program {
     public static void main(String args[]) {
     int numbers [] =  new int [2];
-        String a = "a";
-        String b = "b";
         try {
             BufferedReader br = new BufferedReader(new FileReader("input.txt"));
         String line;
             while ((line = br.readLine()) != null) {
                 if (line.contains("a")){
-                    char Buff = line.charAt(1);
-                    numbers[0] =  Character.digit(Buff, 10);
+                    String Buff = line.substring(1);
+                    numbers[0] =  Integer.parseInt(Buff);
                 }
-                if (line.contains(b)){
-                    char Buff = line.charAt(1);
-                    numbers[1] =  Character.digit(Buff, 10);
+                if (line.contains("b")){
+                    String Buff = line.substring(1);
+                    numbers[1] =  Integer.parseInt(Buff);
                 }
             }
         } catch (Exception e) {
             System.out.println("Error: "+ e);
         }
         double result = Math.pow((double)numbers[0], (double)numbers[1]);
-        int res = (int) result;
+        Double res =  result;
         if (res == 0){
             System.out.println("Не определено");
         }
